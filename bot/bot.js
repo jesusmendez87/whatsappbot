29b997923dde2c6b4aaf5d7c07af1239e63ca88a
jesusmendez87 <alerta.acuario@gmail.com>
2026-04-16 22:32:05 +0200

    new file:   backend/app/Providers/RouteServiceProvider.php 	modified:   bot/bot.js

--- bot/bot.js
+++ bot/bot.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export async function sendToLaravel(messageData) {
   try {
-    const res = await axios.post("http://localhost:8000/api/bot/webhook", {
+    const res = await axios.post("https://whatsappbot-laravel.onrender.com/api/bot/webhook", {
       phone: messageData.from,
       message: messageData.body,
       name: messageData.pushName || null
